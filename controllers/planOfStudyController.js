15a3f389008c5315d257f9803b4b1925592af024
Nicolas Reyes <147343130+TheHanShotFirst@users.noreply.github.com>
2025-03-26 16:28:38 -0400

    addSemester, deleteSemester, and searchSemester endpoints

--- controllers/planOfStudyController.js
+++ controllers/planOfStudyController.js
@@ -1,6 +1,6 @@
 const PlanOfStudy = require('../models/PlanOfStudy');
 const Course = require("../models/Course");
-
+const mongoose = require('mongoose');
 
 const addCourseToPlan = async (req, res) => {
     try {
@@ -90,8 +90,6 @@ const getAvailableCourses = async (req, res) => {
 };
 
 
-
-
 const createPlanOfStudy = async (req, res) => {
     try {
         const { studentId, semesters, totalCredits } = req.body;
@@ -113,5 +111,168 @@ const createPlanOfStudy = async (req, res) => {
     }
 };
 
-module.exports = { addCourseToPlan, getUserPlanOfStudy, getAvailableCourses, createPlanOfStudy };
+// Create Semester (POST)
+const addSemester = async (req, res) => {
+    try {
+        
+        const { userId, semesterName, year } = req.body;
+        
+        // if name is not valid
+        if(semesterName !== "Spring" && semesterName !== "Summer" && semesterName !== "Fall")
+        {
+            return res.status(400).json({ error: "Invalid semester (Valid Options: Spring, Summer, Fall)" });
+        }
+
+        if(year < 2025)
+        {
+            return res.status(400).json({ error: "Invalid year (Must be 2025 or later)" });
+        }
+
+        // Find the user's Plan of Study
+        let plan = await PlanOfStudy.findOne({ studentId: new mongoose.Types.ObjectId(userId) });
+
+        if (!plan) {
+            return res.status(404).json({ error: "Plan of Study not found." });
+        }
+
+        // Now we need to add another semester index
+        // if empty, no checking needed add whatever is passed
+        if(plan.semesters.length === 0)
+        {
+            plan.semesters.push(
+            {
+                semester: semesterName, 
+                year: year, 
+                courses: [] // empty courses
+            });
+        }
+        // else we need to calculate what semester it is and year
+        else
+        {
+            // need to check if semester and year are valid based off previous semester
+            if(plan.semesters[plan.semesters.length-1].semester === "Fall" && plan.semesters[plan.semesters.length-1].year === year)
+            {
+                return res.status(400).json({ error: "Invalid semester and year combination" });
+            }
+            else if(plan.semesters[plan.semesters.length-1].semester === "Spring" && semesterName === "Spring" && plan.semesters[plan.semesters.length-1].year === year) 
+            {
+                return res.status(400).json({ error: "Invalid semester and year combination" });
+            }
+            else if(plan.semesters[plan.semesters.length-1].semester === "Summer" && (semesterName === "Spring" || semesterName === "Summer") && plan.semesters[plan.semesters.length-1].year === year)
+            {
+                return res.status(400).json({ error: "Invalid semester and year combination" });
+            }
+
+            // valid semester/year combo
+            plan.semesters.push(
+            {
+                semester: semesterName,
+                year: year,
+                courses: []
+            });
+        }
+
+        // send response
+        await plan.save();
+        res.json(plan);
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+// Delete Semester (DELETE)
+const deleteSemester = async (req, res) => {
+    try {
+            // get user id and semester id from parameter
+            const userId = req.params.userId;
+            const semesterId = req.params.semesterId;
+    
+            // find plan of study semester to be deleted is in
+            const plan = await PlanOfStudy.findOne({ studentId: new mongoose.Types.ObjectId(userId) });
+
+            // no plan of study
+            if (!plan)
+            {
+                return res.status(404).json({ error: 'Plan of Study not found.' });
+            }
+            
+            // find semester index
+            let j = -1;
+            for(let i = 0; i < plan.semesters.length; i++)
+            {
+                if(plan.semesters[i]._id.equals(new mongoose.Types.ObjectId(semesterId)))
+                {
+                    j = i;
+                    plan.semesters.splice(i,1); // remove semester from array
+                    break;
+                }
+            }
+
+            // if nothing was found
+            if(j < 0)
+            {
+                return res.status(404).json({ error: 'Semester not found.' });
+            }
+
+            // Successful
+            await plan.save();
+            res.json({ message: 'Semester deleted successfully!' });
+
+        } catch (err) {
+            res.status(500).json({ error: err.message });
+        }
+}
+
+// Search for Semester (POST)
+const searchSemester = async (req, res) => {
+    try {
+
+        // semesters with same semester name
+        function checkName(semester){
+            return semester.semester.includes(search);
+        }
+
+        // semesters with same year
+        function checkYear(semester){
+            return semester.year === Number(search);
+        }
+
+        // semester with same name/year
+        function checkSearch(semester){
+            return semester.semester.includes(search.split(" ")[0]) && semester.year === Number(search.split(" ")[1]); 
+        }
+
+        const { userId, search, mode} = req.body;
+
+        // find plan of study we are searching
+        const plan = await PlanOfStudy.findOne({ studentId: new mongoose.Types.ObjectId(userId) });
+
+        // no plan of study
+        if (!plan)
+        {
+            return res.status(404).json({ error: 'Plan of Study not found.' });
+        }
+
+        let result = [];
+        if(mode === 0) // searching for name
+        {
+            result = plan.semesters.filter(checkName);
+        } else if (mode === 1) // searching for year
+        {
+            result = plan.semesters.filter(checkYear);
+        } else if (mode === 2) // searching for one name + year
+        {
+            result = plan.semesters.filter(checkSearch);
+        }
+
+        res.json({ semesters: result });
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+
+}
+
+module.exports = { addCourseToPlan, getUserPlanOfStudy, getAvailableCourses, createPlanOfStudy, addSemester, deleteSemester, searchSemester };
 
