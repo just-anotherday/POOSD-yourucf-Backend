5ae4a94fc06e2e2ff6d6d7c999733c0debf7d39f
Nicolas Reyes <147343130+TheHanShotFirst@users.noreply.github.com>
2025-03-26 16:29:38 -0400

    needed routes for addSemester, deleteSemester, and searchSemester

--- routes/planOfStudyRoutes.js
+++ routes/planOfStudyRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const PlanOfStudy = require('../models/PlanOfStudy'); 
-const { addCourseToPlan, getUserPlanOfStudy, getAvailableCourses, createPlanOfStudy } = require('../controllers/planOfStudyController');
+const { addCourseToPlan, getUserPlanOfStudy, getAvailableCourses, createPlanOfStudy, addSemester, deleteSemester, searchSemester } = require('../controllers/planOfStudyController');
 
 console.log("✅ planOfStudyRoutes.js Loaded");
 
@@ -16,5 +16,14 @@ router.post('/add-course', addCourseToPlan);
 
 // Create a new Plan of Study: This enables the creation of a plan of study using a POST request
 router.post("/create", createPlanOfStudy);
+
+// Add a semester to a plan of study
+router.post("/addSemester", addSemester);
+
+// Remove a semester from a plan of study
+router.delete('/deleteSemester/:semesterId/user/:userId', deleteSemester);
+
+// Search for a semester with matching name, year, or both depending on mode
+router.post('/searchSemester', searchSemester);
 
 module.exports = router;
